2a14de13dd7fd5178d8816696038b113f7e63abf
JakubPilarski <250991@edu.p.lodz.pl>
2024-05-03 16:56:15 +0200

    array

---
+++ array.h
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 03.05.2024.
+//
+
+#ifndef UNTITLED11_ARRAY_H
+#define UNTITLED11_ARRAY_H
+void  enterValue(int *array);
+void printArray(int array[]);
+int findMaximumValue(int array[]);
+int findMiniumValue(int array[]);
+float calculateAverage(int array[]);
+void fileenter(int array[]);
+void readFile(int array[]);
+#endif //UNTITLED11_ARRAY_H
